137f24204ef1b253cfc7989bf3f44f0e85dea510
Nhm Nazmul <najmulmonir12345@gmail.com>
2026-06-19 20:45:38 +0600

    project init

---
+++ app/layout.tsx
@@ -0,0 +1,29 @@
+import { DM_Sans } from "next/font/google"
+
+import "./globals.css"
+import { ThemeProvider } from "@/components/theme-provider"
+import { cn } from "@/lib/utils"
+import Navbar from "@/components/layout/Navbar/Navbar"
+
+const DMSans = DM_Sans({ subsets: ["latin"], variable: "--font-dm-sans" })
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+      className={cn("antialiased", DMSans.variable)}
+    >
+      <body className="bg-background">
+        <ThemeProvider>
+          <Navbar />
+          <main>{children}</main>
+        </ThemeProvider>
+      </body>
+    </html>
+  )
+}
---
+++ app/page.tsx
@@ -0,0 +1,3 @@
+export default function Page() {
+  return <div className="flex min-h-svh p-6">Home page</div>
+}
